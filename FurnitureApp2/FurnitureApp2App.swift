0fbf7f94374f71051a622ebe5ecbe3b364b2534a
Abdulaziz Al_Dukhayni <az.aldukhayni@gmail.com>
2022-12-26 12:23:24 -0500

    Initial Commit

---
+++ FurnitureApp2/FurnitureApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureApp2App.swift
+//  FurnitureApp2
+//
+//  Created by admin on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
