78b0f83ee709bfb59e3a085b7f831f5b2c05621f
Abdulaziz Al_Dukhayni <az.aldukhayni@gmail.com>
2022-12-26 12:45:36 -0500

    Add new View Sign in

---
+++ FurnitureApp2/Views/Signin.swift
@@ -0,0 +1,20 @@
+//
+//  Signin.swift
+//  FurnitureApp2
+//
+//  Created by admin on 12/26/22.
+//
+
+import SwiftUI
+
+struct Signin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Signin_Previews: PreviewProvider {
+    static var previews: some View {
+        Signin()
+    }
+}
